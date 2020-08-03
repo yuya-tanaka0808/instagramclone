28fc0c01fcc51ff1da3832acc4959e39e89330c7
yuya tanaka <yuya.tanaka0808.w@gmail.com>
2020-08-03 19:52:52 +0900

    アプリ制作

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  before_validation { email.downcase! }
+  validates :name,  presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+end
